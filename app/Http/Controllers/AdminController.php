131fceabb1d044ba4245fa63ccce70da4088862a
Harpreet Singh <singh.harpreet89@hotmail.com>
2019-03-20 16:19:53 -0400

    Added Admin Dashboard, Admin posts, Admin Comments

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePost;
+use App\Post;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -19,7 +21,32 @@ class AdminController extends Controller
 
     public function posts()
     {
-        return view('admin.posts');
+        $posts = Post::all();
+        return view('admin.posts', compact('posts'));
+    }
+
+    public function postEdit($id)
+    {
+        $post = Post::where('id', $id)->first();
+        return view('admin.editPost', compact('post'));
+    }
+
+    public function postEditPost(CreatePost $request, $id)
+    {
+        $post = Post::where('id', $id)->first();
+        $post->title = $request['title'];
+        $post->content = $request['content'];
+        $post->save();
+
+        return back()->with('success', 'Post updated successfully');
+    }
+
+    public function deletePost($id)
+    {
+        $post = Post::where('id', $id)->first();
+        $post->delete();
+
+        return back();
     }
 
     public function comments()
--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -1,1 +1,52 @@
 @extends('layouts.admin')
+
+@section('title') Admin Posts @endsection
+
+@section('content')
+    <div class="content">
+        <div class="card">
+            <div class="card-header bg-light">
+                Admin Posts
+            </div>
+
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Created at</th>
+                            <th>Updated at</th>
+                            <th>Comments</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($posts as $post)
+                            <tr>
+                                <td>{{ $post->id }}</td>
+                                <td class="text-nowrap"><a href="{{ route('singlePost', $post->id) }}">{{ $post->title }}</a></td>
+                                <td>{{ \Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</td>
+                                <td>{{ \Carbon\Carbon::parse($post->updated_at)->diffForHumans() }}</td>
+                                <td>{{ $post->comments->count() }}</td>
+                                <td class="form-row align-items-center">
+                                    <a href="{{ route('adminPostEdit', $post->id) }}" class="btn btn-warning col-sm-3 mr-1">Edit</a>
+
+                                    <form id="adminDeletePost-{{ $post->id }}" action="{{ route('adminDeletePost', $post->id) }}" method="POST">
+                                        @csrf
+                                    </form>
+                                    <a href="#" class="btn btn-danger col-sm-3 ml-1" onclick="document.getElementById('adminDeletePost-{{ $post->id }}').submit()">Remove</a>
+                                </td>
+                                <td>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
